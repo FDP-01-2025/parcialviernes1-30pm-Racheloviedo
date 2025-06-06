6867a405a192de828e4fe13380fa3acf3912fdc3
RachelOviedo <00033125@uca.edu.sv>
2025-06-06 14:33:51 -0600

    Agregando parcial. cpp

---
+++ parcial.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+//Ejercicio 1 While, do-while
+int main(){
+
+    int number, n;
+    do {
+        cout << "Por favor ingrese numeros enteros positivos." << endl;
+        cin >> number;
+    } while(number > 0);
+
+    cout << "Cantidad de numeros ingresados: " << endl;
+    for(int i = 0; i < number; i++){
+        cout << i << endl;
+    }
+
+//Ejercicio 2
+int altura; 
+    cout << "Ingrese la altura que desea que tenga el triangulo." << endl;
+    cin >> altura;
+    for(int i = 0; i < altura; i++){
+        cout << i << endl;
+
+    }
+
+    return 0;
+}
