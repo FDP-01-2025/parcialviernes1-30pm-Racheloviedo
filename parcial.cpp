a2703c6e0eb84203f91c0ff6c04b8de0c01d86a4
RachelOviedo <00033125@uca.edu.sv>
2025-06-06 15:40:41 -0600

    Agregando cambios

--- parcial.cpp
+++ parcial.cpp
@@ -9,9 +9,10 @@ int main(){
         cout << "Por favor ingrese numeros enteros positivos." << endl;
         cin >> number;
     } while(number > 0);
-    cout << "Cantidad de numeros ingresados: " << endl;
+   
     for(int i = 0; i < number; i++){
         cout << i << endl;
+        cout << "Cantidad de numeros ingresados: " << endl;
     }
 
     
@@ -20,10 +21,47 @@ int main(){
 int altura; 
     cout << "Ingrese la altura que desea que tenga el triangulo." << endl;
     cin >> altura;
-    for(int j = 0; j < altura; j++){
+    for(int j = 1; j <= altura; j++){
         cout << j << endl;
-
     }
+
+    //Extra 2
+    int menuGlobal, carro;
+    do{
+        cout << "Menu de Opciones." << endl;
+    cout << "1. ver productos disponibles." << endl;
+    cout << "2. Agregar producto al carrito." << endl;
+    cout << "3. Ver carrito de compras." << endl;
+    cout << "4. Finalizar compra y salir." << endl;
+    cin >> menuGlobal;
+    switch(menuGlobal){
+        case 1:
+        cout << "Productos disponibles." << endl;
+        cout << "1. Paginas ($0.25)." << endl;
+        cout << "2. Caja de colores ($1.00)." << endl;
+        cout << "3. pegamento. ($0.75)" << endl;
+        break;
+        case 2:
+        cout << "Agregar producto." << endl;
+        cout << "1. Paginas ($0.25)." << endl;
+        cout << "2. Caja de colores ($1.00)." << endl;
+        cout << "3. pegamento. ($0.75)" << endl;
+        cin >> carro;
+        break;
+        case 3:
+        cout << "Carro: " << carro << endl;;
+        break;
+        case 4:
+        cout << "Productos disponibles." << endl;
+        cout << "1. Paginas ($0.25)." << endl;
+        cout << "2. Caja de colores ($1.00)." << endl;
+        cout << "3. pegamento. ($0.75)" << endl;
+        break;
+    }
+
+    } while (menuGlobal != 4);
+    
+
 
 
 
