1307f1a7c787c214965aa189f3542f8ba59a723d
RachelOviedo <00033125@uca.edu.sv>
2025-06-06 15:22:30 -0600

    agregar cambio a parcial.cpp

--- parcial.cpp
+++ parcial.cpp
@@ -9,20 +9,23 @@ int main(){
         cout << "Por favor ingrese numeros enteros positivos." << endl;
         cin >> number;
     } while(number > 0);
-
     cout << "Cantidad de numeros ingresados: " << endl;
     for(int i = 0; i < number; i++){
         cout << i << endl;
     }
 
+    
+
 //Ejercicio 2
 int altura; 
     cout << "Ingrese la altura que desea que tenga el triangulo." << endl;
     cin >> altura;
-    for(int i = 0; i < altura; i++){
-        cout << i << endl;
+    for(int j = 0; j < altura; j++){
+        cout << j << endl;
 
     }
+
+
 
     return 0;
 }
